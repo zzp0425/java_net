6061cb91966645eddadb4016eed58d8f3e3513b1
zzp0425 <412960861@qq.com>
2016-03-22 23:33:49 +0800

    first compile

--- src/com/network/thread/DigestThread.java
+++ src/com/network/thread/DigestThread.java
@@ -1,8 +1,52 @@
 package com.network.thread;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by zzp on 2016/3/21.
  */
 public class DigestThread extends Thread {
     private String fileName;
+
+    public DigestThread(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        try {
+            FileInputStream in = new FileInputStream(fileName);
+            MessageDigest sha = MessageDigest.getInstance("SHA-256");
+            DigestInputStream dis = new DigestInputStream(in, sha);
+            while (dis.read() != -1) {
+
+            }
+            dis.close();
+
+            byte[] digest = sha.digest();
+
+            StringBuilder result = new StringBuilder(fileName);
+            result.append(": ");
+            result.append(DatatypeConverter.printHexBinary(digest));
+            System.out.println(result);
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] files = new String[]{"D:\\trans\\data_9.txt", "D:\\trans\\data_err.txt", "D:\\trans\\data_err_9.txt", "D:\\trans\\list.txt"};
+        for (String fileName : files) {
+            Thread t = new DigestThread(fileName);
+            t.start();
+        }
+    }
 }
