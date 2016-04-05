54988ee5c9559acb202c925560e0e25c32fd7d6f
zzp0425 <412960861@qq.com>
2016-04-05 23:15:13 +0800

    非阻塞I/O 客户端实例1

---
+++ src/com/network/nio/ChargenClient.java
@@ -0,0 +1,36 @@
+package com.network.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * Created by 周振平
+ * on 2016/4/5.
+ */
+public class ChargenClient {
+    public static void main(String[] args) {
+        int port = 19;
+        String host = "rama.poly.edu";
+
+        SocketAddress address = new InetSocketAddress(host, port);
+        try {
+            SocketChannel client = SocketChannel.open(address);
+
+            ByteBuffer buffer = ByteBuffer.allocate(74);
+            WritableByteChannel out = Channels.newChannel(System.out);
+
+            while (client.read(buffer) != -1) {
+                buffer.flip();
+                out.write(buffer);
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
